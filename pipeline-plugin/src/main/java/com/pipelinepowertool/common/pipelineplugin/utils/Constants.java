a83b3a1cae069d44491b7f99c032aae76e2b8aba
Sven den Boer <denboer.sven@gmail.com>
2023-07-18 21:03:22 +0200

    name of constant

--- pipeline-plugin/src/main/java/com/pipelinepowertool/common/pipelineplugin/utils/Constants.java
+++ pipeline-plugin/src/main/java/com/pipelinepowertool/common/pipelineplugin/utils/Constants.java
@@ -2,11 +2,9 @@ package com.pipelinepowertool.common.pipelineplugin.utils;
 
 public class Constants {
 
-    public static final String ENERGY_READER_URL_DEFAULT = "https://energy-reader.s3.eu-north-1.amazonaws.com/energy_reader-default";
-    public static final String ENERGY_READER_URL_JENKINS_ALPINE = "https://energy-reader.s3.eu-north-1.amazonaws.com/energy_reader-jenkins-alpine";
+    public static final String ENERGY_READER_URL_STANDARD = "https://energy-reader.s3.eu-north-1.amazonaws.com/energy_reader-standard";
+    public static final String ENERGY_READER_URL_ALPINE = "https://energy-reader.s3.eu-north-1.amazonaws.com/energy_reader-alpine";
     public static final String ENERGY_READER_FILENAME = "energy_reader";
-
-    public static final String ENERGY_READER_FOLDER = "/tmp/";
     public static final String ENERGY_READINGS_CSV = "EnergyReadings.csv";
 
 }
